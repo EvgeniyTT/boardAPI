ce08ae94104fc5ec24e685e01e04f25752184973
Evgeniy Tatarin <tatarin.e.m@gmail.com>
2016-10-25 20:03:22 +0300

    broadcast board data on change

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -9,18 +9,26 @@ module.exports = (io) => {
       console.log(data);
     });
     socket.on('getBoardData', (data) => {
-      console.log('BOARD DATA WAS REQUESTED');
-      console.log('boardData_1: ', boardData_1);
       socket.emit('getBoardData_235634745', boardData_1);
     })
+    socket.on('boardUpdate', (data) => {
+      console.log('id: ', id);
+      data._id = id
+      console.log(data.boardData[0].tasks[0].name);
+      Board.update({ _id: data._id }, data, function (err, board) {
+        // if (err) return handleError(err);
+          io.emit('getBoardData_235634745', data);
+      })
 
-    setInterval( function() {
-      socket.emit('getBoardData_235634745', boardData_1);
-    }, 1000)
+    })
+
+
   });
   let boardData_1;
+  let id;
   Board.findOne({}, function (err, board) {
     // if (err) return handleError(err);
     boardData_1 = board;
+    id = board._id;
   })
 }
