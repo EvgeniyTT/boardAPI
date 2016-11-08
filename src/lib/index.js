007a07ad7adb84423b184f42e5d07bdacb626035
Evgeniy Tatarin <tatarin.e.m@gmail.com>
2016-11-08 15:34:05 +0200

    delete unused files

--- src/lib/index.js
+++
@@ -1,7 +0,0 @@
-import ng from 'angular';
-import taskPanelDirective from './directive';
-import taskCard from '../task.card';
-
-export default ng.module('taskPanel', [taskCard])
-  .directive('taskPanel', taskPanelDirective)
-  .name;
