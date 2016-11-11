2f408a8084dc7c7ba45e23edee3d992667464963
Evgeniy Tatarin <tatarin.e.m@gmail.com>
2016-11-11 12:54:22 +0200

    add auth token

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -8,6 +8,14 @@ const ObjectId = require('mongoose').Types.ObjectId;
 const Board = db.model('Board');
 const User = db.model('User');
 
+const notATokenSectet = 'd3hA&bhYh2tn72bXvb'
+
+function makeToken(user) {
+  return jwt.sign(user, notATokenSectet, {
+    expiresIn: '24h'
+  });
+}
+
 module.exports = (io) => {
 
   io.on('connection', function (socket) {
@@ -19,7 +27,8 @@ module.exports = (io) => {
       } else {
         const newUser = new User(user);
         const savedUser = await newUser.save();
-        socket.emit('logged', savedUser);
+        let token = makeToken(user);
+        socket.emit('logged', savedUser, token);
       }
     })
 
@@ -27,65 +36,75 @@ module.exports = (io) => {
       try {
         const user =  await User.findOne({username: data.username, password: data.password});
         if (user) {
-          socket.emit('logged', user);
+          let token = makeToken(user);
+          socket.emit('logged', user, token);
         } else {
           socket.emit('loginError', 'Wrong username or password');
         }
-
       } catch(err) {
-        socket.emit('loginError', err);
+        socket.emit('loginError', 'Some internal error occured, please contact support team.');
       }
     })
 
-    socket.on('getBoardList', async (user) => {
+    socket.on('getBoardList', async (token) => {
       try {
+        const decode = jwt.verify(token, notATokenSectet);
+        const user = decode._doc;
         const publicBoards =  await Board.find({ restriction: "public" }, { name: 1 });
         const privatBoards =  await Board.find({ users: user._id }, { name: 1 });
         socket.emit('boardList', {public: publicBoards, private: privatBoards});
       } catch(err) {
-        socket.emit('error', err);
+        socket.emit('internalError', err);
       }
     })
 
-    socket.on('addBoard', async (board, user_ID) => {
+    socket.on('addBoard', async (token, board) => {
       try {
+        const decode = jwt.verify(token, notATokenSectet);
+        const user = decode._doc;
         const newBoard = new Board (board);
         await newBoard.save();
         socket.join(board._id);
         socket.emit('boardData', board);
       } catch(err) {
-        socket.emit('error', err);
+        socket.emit('internalError', err);
       }
     })
 
-    socket.on('getBoard', async (boardID) => {
+    socket.on('getBoard', async (token, boardID) => {
       try {
+        const decode = jwt.verify(token, notATokenSectet);
+        const user = decode._doc;
         socket.join(boardID);
         const board = await Board.findOne({_id : boardID});
         socket.emit('boardData', board);
       } catch(err) {
-        socket.emit('error', err);
+        socket.emit('internalError', err);
       }
     })
 
-    socket.on('updateBoard', async (board) => {
+    socket.on('updateBoard', async (token, board) => {
       try {
+        const decode = jwt.verify(token, notATokenSectet);
+        const user = decode._doc;
         await Board.update({ _id: new ObjectId(board._id) }, board, {upsert:true});
         io.to(board._id).emit('boardData', board);
       } catch(err) {
-        socket.emit('error', err);
+        socket.emit('internalError', err);
       }
     })
 
-    socket.on('deleteBoard', async (boardID, userID) => {
+    socket.on('deleteBoard', async (token, boardID) => {
       try {
+        const decode = jwt.verify(token, notATokenSectet);
+        const user = decode._doc;
         const result = await Board.remove({ _id: boardID });
         io.to(boardID).emit('boardData', result);
         const publicBoards =  await Board.find({ restriction: "public" }, { name: 1 });
-        const privatBoards =  await Board.find({ users: userID }, { name: 1 });
+        const privatBoards =  await Board.find({ users: user._id }, { name: 1 });
         socket.emit('boardList', {public: publicBoards, private: privatBoards});
       } catch(err) {
-        socket.emit('error', err);
+        socket.emit('internalError', err);
       }
     })
 
