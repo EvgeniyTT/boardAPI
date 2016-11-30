73e2561d6bf3d637d117f822fba97ef1a5a19d02
Evgeniy Tatarin <tatarin.e.m@gmail.com>
2016-11-30 11:10:44 +0200

    add cors

--- src/app.js
+++ src/app.js
@@ -4,6 +4,21 @@ const cors = require('cors');
 
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// app.all('*', function(req, res, next) {
+//      var origin = req.get('origin'); 
+//      res.header('Access-Control-Allow-Origin', origin);
+//      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//      res.header('Access-Control-Allow-Headers', 'Content-Type');
+//      next();
+// });
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   const err = new Error('Not Found');
